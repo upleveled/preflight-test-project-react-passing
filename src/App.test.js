0c6a178ca80636b13012a394da6d6b5b45399dbb
Karl Horky <karl.horky@gmail.com>
2023-05-06 00:03:09 +0200

    Fix @testing-library/jest-dom types properly

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  // @ts-ignore -- Getting proper types for @testing-library/jest-dom is not simple https://github.com/testing-library/jest-dom/issues/123#issuecomment-1536490617
   expect(linkElement).toBeInTheDocument();
 });
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+import { expect } from '@jest/globals';
+import { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
+
+declare module 'expect' {
+  interface AsymmetricMatchers {}
+  interface Matchers<R = void>
+    extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
+}
